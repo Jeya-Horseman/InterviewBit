2e2293c50fee46c667b538b5c3bbc0f9763f3ddb
Jeya Prakash <102975465+Jeya-Horseman@users.noreply.github.com>
2022-08-26 12:47:45 +0530

    Delete mysite directory

--- mysite/mysite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('C:\Users\yummy\PycharmProjects\mysite\members', include('members.urls')),
-    path('C:\Users\yummy\PycharmProjects\mysite\members\admin.py', admin.site.urls),
-]
