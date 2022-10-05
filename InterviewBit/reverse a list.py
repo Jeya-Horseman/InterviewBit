057e9ca8eb3ef0e5c8792ca23dc024e1606a0b07
Jeya Horseman <jeyaprakashse@gmail.com>
2022-10-05 18:48:02 +0530

    v1.1

--- InterviewBit/reverse a list.py
+++ InterviewBit/reverse a list.py
@@ -1,19 +1,19 @@
-l=[1,2,3,4,5,6,7,8,9,10]
+l=[1,2,3,4,5,6,7,8,9,10,11,12,6]
 mid=len(l)//2
 output=[]
 
-for i in range(1,len(l)):
+for i in l:
     if i<mid+1:
         output.append(i)
         continue
     else:
         for j in l[::-1]:
-            if j!=mid:
+            if l[j]!=l[i]:
                 output.append(j)
                 continue
             break
     break
-print(output)    
+print(output)
 
 
 
--- InterviewBit/sum of numbers.py
+++ InterviewBit/sum of numbers.py
@@ -1,11 +1,8 @@
-num = 29
+num = 3
 flag = False
-
 if num > 1:
-    # check for factors
     for i in range(2, num):
         if (num % i) == 0:
-            # if factor is found, set flag to True
             flag = True
             break
 if flag:
